75a9cb06cdd5855b1b7e003b7c945bf29058a0fc
Maksim Kot <kotmaksimarturovich@gmail.com>
2025-01-18 23:33:17 +0300

    Added the RatingEvent structure to begin implementing asynchronous requests

--- rating/pkg/model/rating.go
+++ rating/pkg/model/rating.go
@@ -27,3 +27,22 @@ type Rating struct {
 	UserID     UserID      `json:"userId"`
 	Value      RatingValue `json:"value"`
 }
+
+// RatingEvent defines an event containing rating information.
+type RatingEvent struct {
+	UserID     UserID          `json:"userId"`
+	RecordID   RecordID        `json:"recordId"`
+	RecordType RecordType      `json:"recordType"`
+	Value      RatingValue     `json:"value"`
+	ProviderID string          `json:"providerId"`
+	EventType  RatingEventType `json:"eventType"`
+}
+
+// RatingEventType defines the type of a rating event.
+type RatingEventType string
+
+// Rating event types.
+const (
+	RatingEventTypePut    = RatingEventType("put")
+	RatingEventTypeDelete = RatingEventType("delete")
+)
